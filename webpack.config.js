ee8fb62d9f0a2193f9f17fac5d056b9b50dc0b71
kassanj <kassandram022@gmail.com>
2019-11-04 17:12:29 -0800

    initial commit

---
+++ webpack.config.js
@@ -0,0 +1,44 @@
+const webpack = require('webpack');
+const path = require('path');
+
+module.exports = {
+  mode: 'development',
+  entry: [
+    './client/index.js',
+  ],
+  output: {
+    path: path.resolve(__dirname, '/dist'),
+    filename: 'bundle.js',
+    publicPath: path.resolve(__dirname, "/dist")
+  },
+  devServer: {
+    hot: true,
+    publicPath: path.resolve(__dirname, '/dist'),
+    proxy: {
+      '/api': 'http://localhost:3000',
+    },
+    historyApiFallback: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react']
+          }
+        }
+      },
+      {
+        test: /\.css$/,
+        use: [
+          "style-loader", // creates style nodes from JS strings
+          "css-loader", // translates CSS into CommonJS
+          "sass-loader" // compiles Sass to CSS, using Node Sass by default
+        ]
+      },
+    ]
+  },
+};
