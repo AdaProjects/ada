a2904c694ce265866cf53b561cd07299617d0fea
Dominic Ameneyro <dominicameneyro@Dominics-MacBook-Pro.local>
2019-11-05 10:47:23 -0800

    updated webpack

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ const path = require('path');
 module.exports = {
   mode: 'development',
   entry: [
-    './client/index.js',
+    './src/index.js',
   ],
   output: {
     path: path.resolve(__dirname, '/dist'),
