6fef73044c94252a592d6c79efe2511c09c55856
Dominic Ameneyro <dominicameneyro@Dominics-MacBook-Pro.local>
2019-11-05 10:17:18 -0800

    app.jsx and index.js set up complete

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,3 +1,24 @@
 import * as React from 'react';
+import React, { Component } from 'react';
 
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      newsDisplay: false,
+      profileDisplay: false,
+      projectDisplay: false,
+      projectFav: false,
+      signedIn: false,
+    }
+  }
+
+  render() {
+    return (
+      <div>
+
+      </div>
+    )
+  }
+}
 // main container for application
