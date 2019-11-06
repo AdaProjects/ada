48e7fa5b3e06b2c12a132f4274477f11d0fa17dc
kassanj <kassandram022@gmail.com>
2019-11-06 15:07:35 -0800

    add styling

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -3,8 +3,9 @@ import * as React from 'react';
 // will have a sign in form, and can conditionally render "sign up" prompts.
 const Login = () => {
   return (
-    <div className="header">
-      Login Stuff
+    <div className="main">
+      <h2>ADA</h2>
+      <p className="login-text">Login here</p>
     </div>
   );
 };
